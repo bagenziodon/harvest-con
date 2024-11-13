cf4a60fe4b269b4ef3d776f170daacb4585ed761
bagenzi <bagenzi.bi@gmail.com>
2024-11-13 16:11:12 +0200

    complete navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,11 @@ import Contact from './pages/contact'
 import MyProfile from './pages/myProfile'
 import MyAppointments from './pages/myAppointments'
 import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]' >
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/doctors' element={<Doctors/>} />
